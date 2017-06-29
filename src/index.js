c645cf216a21e7588f0e46e47a2900d8c89825f6
Sergio Lucha <slucha@hotmail.com>
2017-06-29 10:25:10 -0600

    actualizacion 29062007

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import "./index.css";
-import numeral from "numeral";
-/*eslint-disable no-console*/
-const courseValue = numeral(1000).format("$0,0.00");
+import {getUsers, deleteUser} from "./api/userApi";
 
-console.log(`I would pay ${courseValue} for this awesome course!`);
+getUsers().then(result =>{
+    let userBody = "";
+    result.forEach(user =>{
+    userBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+    })
+    global.document.getElementById("users").innerHTML = userBody;    
+
+    const deleteLinks = global.document.getElementsByClassName("deleteUser");
+    Array.from(deleteLinks, link => {
+      link.onclick = function(event){
+        const element = event.target;
+        event.preventDefault();
+        deleteUser(element.attributes["data-id"].value);
+        const row = element.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+      }
+    })
+}) ;
+
